b9c1e9fa524893ea6fce22eacc4366895621a5c1
Daphne Mol <533314@student.glu.nl>
2020-04-02 21:49:50 +0200

    Added img slider

--- views/index.php
+++ views/index.php
@@ -8,7 +8,7 @@ require_once '../controllers/session.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Webshop</title>
+    <title>SHOESHOE</title>
     <link rel="stylesheet" href="../assets/css/index.css">
 </head>
 
@@ -24,6 +24,9 @@ require_once '../controllers/session.php';
                 <?php if (isset($_SESSION['admin'])) { ?>
                     <li><a href="admin/product/product_overzicht.php">Admin Product</a></li>
                     <li><a href="admin/category/category_overzicht.php">Admin Category</a></li>
+                    <li><a href="admin/admin/admin_overzicht.php">Admin Admins</a></li>
+                    <li><a href="admin/customer/customer_overzicht.php">Admin Customer</a></li>
+                    <li><a href="admin/user/user_overzicht.php">Admin User</a></li>
                 <?php } ?>
                 <?php if (!isset($_SESSION['username'])) { ?>
                     <li style="float: right"><a href="login.php">Inloggen</a></li>
@@ -35,10 +38,69 @@ require_once '../controllers/session.php';
             </ul>
         </nav>
         <main>
-            <p>Test</p>
-        </main>
-    </div>
 
+            <div class="slideshow-container">
+                <div class="mySlides fade">
+                    <img src="../assets/img/slider/store1.jpg" style="width:90%">
+                </div>
+
+                <div class="mySlides fade">
+                    <img src="../assets/img/slider/store2.jpg" style="width:90%">
+                </div>
+
+                <div class="mySlides fade">
+                    <img src="../assets/img/slider/store3.jpg" style="width:90%">
+                </div>
+
+                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+
+            </div>
+            <br>
+
+            <div style="text-align:center">
+                <span class="dot" onclick="currentSlide(1)"></span>
+                <span class="dot" onclick="currentSlide(2)"></span>
+                <span class="dot" onclick="currentSlide(3)"></span>
+            </div>
+        </main>
+        <footer>
+            <small>&copy; Copyright 2020, SHOESHOE</small>
+        </footer>
+    </div>
+    <script>
+        var slideIndex = 1;
+        showSlides(slideIndex);
+
+        function plusSlides(n) {
+            showSlides(slideIndex += n);
+        }
+
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
+
+        function showSlides(n) {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            var dots = document.getElementsByClassName("dot");
+            if (n > slides.length) {
+                slideIndex = 1
+            }
+            if (n < 1) {
+                slideIndex = slides.length
+            }
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " active";
+        }
+    </script>
 </body>
 
 </html>
