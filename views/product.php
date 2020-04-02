4ad679b0aaa11bd5bc2a51c7bd15064c04413d0e
Daphne Mol <533314@student.glu.nl>
2020-04-02 21:50:37 +0200

    Made it dynamic with a database

--- views/product.php
+++ views/product.php
@@ -1,5 +1,16 @@
 <?php
 require_once '../controllers/session.php';
+require_once '../controllers/connect.php';
+
+$qry = $conn->query("SELECT * FROM product");
+
+$products = [];
+
+while ($row = $qry->fetch_assoc()) {
+    array_push($products, $row);
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -22,76 +33,15 @@ require_once '../controllers/session.php';
             </ul>
         </nav>
         <main>
-            <div class="card">
-                <img src="../assets/img/shoes/j1.png" alt="shoes" style="width:100%">
-                <h1>Jordan 1 Mid</h1>
-                <p class="price">&euro;119.99</p>
-                <p>Mooie groene schoenen</p>
-                <p><button>Add to Cart</button></p>
-            </div>
-            <div class="card">
-                <img src="../assets/img/shoes/am1.png" alt="Denim Jeans" style="width:100%">
-                <h1>Nike Am 97 Celebration Of The Swoosh Cos</h1>
-                <p class="price">&euro;99.99</p>
-                <p>Mooie witte schoenen</p>
-                <p><button>Add to Cart</button></p>
-            </div>
-            <div class="card">
-                <img src="../assets/img/shoes/v1.png" alt="Denim Jeans" style="width:100%">
-                <h1>Vans Authentic</h1>
-                <p class="price">&euro;79.99</p>
-                <p>Mooie grijze schoenen</p>
-                <p><button>Add to Cart</button></p>
-            </div>
-            <div class="card">
-                <img src="../assets/img/shoes/b1.png" alt="Denim Jeans" style="width:100%">
-                <h1>Buffalo Crevis</h1>
-                <p class="price">&euro;99.99</p>
-                <p>Mooie roze schoenen</p>
-                <p><button>Add to Cart</button></p>
-            </div>
-            <div class="card">
-                <img src="../assets/img/shoes/n1.png" alt="Denim Jeans" style="width:100%">
-                <h1>Nike Air Max 2090</h1>
-                <p class="price">&euro;149,99</p>
-                <p>Mooie witte schoenen</p>
-                <p><button>Add to Cart</button></p>
-            </div>
-            <div class="card">
-                <img src="../assets/img/shoes/air1.png" alt="Denim Jeans" style="width:100%">
-                <h1>Nike Air Max 270</h1>
-                <p class="price">&euro;149,99</p>
-                <p>Mooie zwarte schoenen</p>
-                <p><button>Add to Cart</button></p>
-            </div>
-            <div class="card">
-                <img src="../assets/img/shoes/oz1.png" alt="Denim Jeans" style="width:100%">
-                <h1>adidas Ozweego Suede</h1>
-                <p class="price">&euro;64,99</p>
-                <p>Mooie grijze schoenen</p>
-                <p><button>Add to Cart</button></p>
-            </div>
-            <div class="card">
-                <img src="../assets/img/shoes/cair1.png" alt="Denim Jeans" style="width:100%">
-                <h1>Nike Air Force 1</h1>
-                <p class="price">&euro;84,99</p>
-                <p>Mooie roze schoenen</p>
-                <p><button>Add to Cart</button></p>
-            </div>
-            <div class="card">
-                <img src="../assets/img/shoes/bn1.png" alt="Denim Jeans" style="width:100%">
-                <h1>Nike Tuned 1</h1>
-                <p class="price">&euro;59,99</p>
-                <p>Mooie grijze schoenen</p>
-                <p><button>Add to Cart</button></p>
-            </div>
-            <div class="card">
-                <img src="../assets/img/shoes/bair1.png" alt="Denim Jeans" style="width:100%">
-                <h1>Nike Air Force 1 Highness</h1>
-                <p class="price">&euro;54,99</p>
-                <p>Mooie paarse schoenen</p>
-                <p><button>Add to Cart</button></p>
-            </div>
+            <?php foreach ($products as $product) { ?>
+                <div class="card">
+                    <img src="../assets/img/shoes/<?= $product['image1'] ?>" alt="<?= $product['name'] ?>" style="width:100%">
+                    <h1><?= $product['name'] ?></h1>
+                    <p class="price">&euro;<?= $product['price'] ?></p>
+                    <p><?= $product['description'] ?></p>
+                    <p><button>Add to Cart</button></p>
+                </div>
+            <?php } ?>
         </main>
         <footer>
             <small>&copy; Copyright 2020, SHOESHOE</small>
